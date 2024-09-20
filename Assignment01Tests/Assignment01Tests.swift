225226b0d0e4aa8f2509b61d339dca8adf2047fc
Marina <marinacarvalho@protonmail.com>
2024-09-20 09:51:00 -0400

    Initial Commit

---
+++ Assignment01Tests/Assignment01Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment01Tests.swift
+//  Assignment01Tests
+//
+//  Created by Marina Carvalho on 2024-09-20.
+//
+
+import Testing
+@testable import Assignment01
+
+struct Assignment01Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
